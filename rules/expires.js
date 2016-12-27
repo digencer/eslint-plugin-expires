7b4aab7d5cfe777230e56286c5c2e988f141c767
DilekGencer <digencer@gmail.com>
2016-12-28 01:55:04 +0300

    Add eslintrc Refactor some messages Update Readme

--- rules/expires.js
+++ rules/expires.js
@@ -1,55 +1,52 @@
 "use strict";
 
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 // Rule Definition
-//------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------
 
 module.exports = function(context) {
-	var tag = "EXPIRES";
-	var invalidTimeError = "Invalid date!";
-	var timeZoneError = "You must define a timezone!";
-	var expiredError = "Expires date!";
-	var currentError = null;
-	var expiredAsMilliseconds = 0;
+  var tag = "EXPIRES";
+  var INVALID_TIME_ERROR = "Invalid date!";
+  var TIMEZONE_ERROR = "You must define a timezone!";
+  var EXPIRED_ERROR = "Date expired!";
+  var CURRENT_ERROR = null;
+  var expiredAsMilliseconds = 0;
 
-	function trimAllSpacesAndStars(str) {
+  function trimAllSpacesAndStars(str) {
     return str.replace(/\*/g, "") // remove astricks
       .replace(/^\s+|\s+$/g, ""); // remove all white space including new lines
   }
 
-	function checkCommentValidity(node) {
+  function checkCommentValidity(node) {
     var value = trimAllSpacesAndStars(node.value);
     if (value.length === 0) {
-      return;
+      return false;
     } else if (value.split(": ")[0] !== tag) {
-      return;
-    } else {
-      return true;
+      return false;
     }
-	}
+    return true;
+  }
 
-	function getExpireDateValidity(node) {
+  function getExpireDateValidity(node) {
     var value = trimAllSpacesAndStars(node.value);
     var expiredDatePlus = value.split(": ")[1];
     var expiredDate = expiredDatePlus.match(/(^\S*)/)[0]; // take all before first space or new line
     var isValidDate = /(\d{4}-\d{2}-\d{2})(T\d{2}:\d{2}?(:\d{1,2})?(.\d{1,3})?(Z|[+-]\d{2}:\d{2})?)?$/.exec(expiredDate);
 
     if (isValidDate === null) {
-      currentError = invalidTimeError;
+      CURRENT_ERROR = INVALID_TIME_ERROR;
       return false;
-    } else if (isValidDate[2] !== undefined && isValidDate[5] === undefined) {
-      currentError = timeZoneError;
+    } else if (isValidDate[2] !== undefined && isValidDate[5] === undefined) { // eslint-disable-line no-undefined
+      CURRENT_ERROR = TIMEZONE_ERROR;
       return false;
-    } else {
-      expiredAsMilliseconds = Date.parse(expiredDate);
-      if (isNaN(expiredAsMilliseconds)) {
-        currentError = invalidTimeError;
-        return false;
-      } else {
-        return true;
-      }
     }
-	}
+    expiredAsMilliseconds = Date.parse(expiredDate);
+    if (isNaN(expiredAsMilliseconds)) {
+      CURRENT_ERROR = INVALID_TIME_ERROR;
+      return false;
+    }
+    return true;
+  }
 
   function displayError(node, reason) {
     context.report(node, reason);
@@ -59,16 +56,16 @@ module.exports = function(context) {
     if (checkCommentValidity(node) && getExpireDateValidity(node)) {
       var currentTime = Date.now();
       if (expiredAsMilliseconds <= currentTime) {
-        displayError(node, expiredError);
+        displayError(node, EXPIRED_ERROR);
       }
-    } else if (currentError !== null) {
-      displayError(node, currentError);
+    } else if (CURRENT_ERROR !== null) {
+      displayError(node, CURRENT_ERROR);
     }
   }
 
   return {
-    "LineComment": checkForExpires,
-    "BlockComment": checkForExpires
+    LineComment: checkForExpires,
+    BlockComment: checkForExpires
   };
 };
 
